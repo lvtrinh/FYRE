f7c3299be3d83ba628a363b9fbf6ce79143ed95d
clayton yamaguchi <ckyamagu@ucsd.edu>
2016-05-22 13:28:24 -0700

    SQLite receipt stuff in ReceiptActivity

--- app/src/main/java/com/teamfyre/fyre/MainActivity.java
+++ app/src/main/java/com/teamfyre/fyre/MainActivity.java
@@ -43,6 +43,7 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.math.BigDecimal;
+import java.util.Iterator;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -113,7 +114,7 @@ public class MainActivity extends AppCompatActivity
 
         // Fetching user details from SQLite
         HashMap<String, String> user = db.getUserDetails();
-        HashMap<String,String> receipt = db.getReceiptDetails();
+        //HashMap<String,String> receipt = db.getReceiptDetails();
 
         String name = user.get("name");
         String email = user.get("email");
@@ -151,12 +152,19 @@ public class MainActivity extends AppCompatActivity
         int orderNumber = testReceipt.getOrderNumber();
         ArrayList<ReceiptItem> itemList = testReceipt.getItemList();
 
-        ReceiptActivity add = new ReceiptActivity();
+        ReceiptActivity add = new ReceiptActivity(db, session);
         int receiptId = 144;
         add.addReceipt(userId, storeName, storeStreet, storeCityState, storePhone, storeWebsite, storeCategory, hereGo, cardType, cardNum, paymentMethod, subtotal, tax, totalPrice, date, time, cashier, checkNumber, orderNumber);
-        db.addReceiptLite(Integer.toString(userId), Integer.toString(receiptId), storeName, storeStreet, storeCityState, storePhone, storeWebsite, storeCategory, Integer.toString(hereGo), cardType, Integer.toString(cardNum), paymentMethod, subtotal.toString(), tax.toString(), totalPrice.toString(), date, time, cashier, checkNumber, Integer.toString(orderNumber));
+        //db.addReceiptLite(Integer.toString(userId), Integer.toString(receiptId), storeName, storeStreet, storeCityState, storePhone, storeWebsite, storeCategory, Integer.toString(hereGo), cardType, Integer.toString(cardNum), paymentMethod, subtotal.toString(), tax.toString(), totalPrice.toString(), date, time, cashier, checkNumber, Integer.toString(orderNumber));
 
-        System.out.println("HERE:" + receipt.get("store_category"));
+        /*Iterator it = receipt.entrySet().iterator();
+        while (it.hasNext()) {
+            HashMap.Entry pair = (HashMap.Entry)it.next();
+            System.out.println(pair.getKey() + " = " + pair.getValue());
+            //it.remove();
+        }
+
+        System.out.println("HERE:" + receipt.get("store_name"));*/
         for (int j = 0; j < itemList.size(); j++) {
             add.addItem(itemList.get(j));
         }
