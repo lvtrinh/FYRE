1f8eef21713143110e1f3dcb52399000e4520210
Leo Trinh <lvtrinh@ucsd.edu>
2016-05-22 12:52:27 -0700

    initial (rough) layout files, adapter file

---
+++ app/src/main/java/com/teamfyre/fyre/ReceiptAdapter.java
@@ -0,0 +1,55 @@
+package com.teamfyre.fyre;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Leo on 5/22/2016.
+ */
+public class ReceiptAdapter extends RecyclerView.Adapter<ReceiptAdapter.RViewHolder> {
+    private List<Receipt> receiptList;
+
+    // ViewHolder class definition
+    public class RViewHolder extends RecyclerView.ViewHolder {
+        public CardView cv;
+        public TextView cv_name, cv_date, cv_price;
+
+        public RViewHolder(View view){
+            super(view);
+            cv = (CardView) view.findViewById(R.id.card_singleton);
+            cv_name = (TextView) view.findViewById(R.id.card_store_name);
+            cv_date = (TextView) view.findViewById(R.id.card_date);
+            cv_price = (TextView) view.findViewById(R.id.card_price);
+        }
+    }
+
+    public ReceiptAdapter(List<Receipt> receiptList) {
+        this.receiptList = receiptList;
+    }
+
+    @Override
+    public RViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.receipt_card_layout, parent, false);
+        return new RViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(RViewHolder holder, int position) {
+        Receipt receipt = receiptList.get(position);
+        holder.cv_name.setText(receipt.getStoreName());
+        holder.cv_date.setText(receipt.getDate());
+        holder.cv_price.setText(receipt.getTotalPrice().toString());
+    }
+
+    @Override
+    public int getItemCount() {
+        return receiptList.size();
+    }
+}
