49ad0106be40f8351ff2a9e172aecc6b9823c829
taylortanita <taylor.tanita@gmail.com>
2016-05-30 19:00:24 -0700

    account activity updated

--- app/src/main/java/com/teamfyre/fyre/AccountActivity.java
+++ app/src/main/java/com/teamfyre/fyre/AccountActivity.java
@@ -1,13 +1,19 @@
 package com.teamfyre.fyre;
 
+import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.view.View;
 import android.util.Log;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -21,9 +27,12 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Created by taylortanita on 5/22/16.
- */
+/******************************************************************************
+ * AccountActivity.java
+ *
+ * This Activity handles a user's account updates such as changes to name, email,
+ * and security question and answer.
+ ******************************************************************************/
 public class AccountActivity extends AppCompatActivity {
 
     private Button UpdateAccount;
@@ -32,26 +41,44 @@ public class AccountActivity extends AppCompatActivity {
     private EditText email1;
     private EditText password;
     private SessionManager session;
+    private Spinner spinner;
+    private ArrayAdapter<CharSequence> adapter;
+    private String selectedQ;
+    private String answer;
+    private int qOption;
+    private boolean ans;
     private SQLiteHandler db;
     private static final String TAG = RegisterActivity.class.getSimpleName();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_account);
 
+        //associates file with screen
+        setContentView(R.layout.activity_account);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //launches SQLite database
         db = new SQLiteHandler(getApplicationContext());
 
         // session manager
         session = new SessionManager(getApplicationContext());
+        spinner = (Spinner) findViewById(R.id.security);
+        adapter = ArrayAdapter.createFromResource(this, R.array.security_questions, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
         // Fetching user details from SQLite
         HashMap<String, String> user = db.getUserDetails();
 
+        //getting user's information to display on page
         final String name = user.get("name");
         final String email = user.get("email");
         final int id = Integer.parseInt(user.get("id"));
 
+        //connecting to xml
         name1 = (EditText) findViewById(R.id.name);
         email1 = (EditText) findViewById(R.id.email);
         password = (EditText) findViewById(R.id.password);
@@ -61,52 +88,36 @@ public class AccountActivity extends AppCompatActivity {
         UpdateAccount.setBackgroundResource(0);
         RemoveAccount.setBackgroundResource(0);
 
+        //setting EditText so that the user's current information is stored there
         name1.setText(name);
         email1.setText(email);
 
+        //updates user's information.  handles if there are null fields or data is unchanged
         UpdateAccount.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 String n = name1.getText().toString();
                 String e = email1.getText().toString();
                 String p = password.getText().toString();
 
-                //checking if email is in correct format
-                /**
-                 int emailLength = e.length();
-                boolean at = false;
-                int atIndex = 0;
-                boolean dot = false;
-                for (int i = 0; i < emailLength; i++) {
-                    if (i > 0 && e.charAt(i) == '@') {
-                        at = true;
-                        atIndex = i;
-                    }
-                    if (i > atIndex + 1 && i > 1 && e.charAt(i) == '.')
-                        dot = true;
-                }
-                 **/
-
-                /**
-                if (n.length() != 0)
-                    updateName(n);
-                if (e.length() != 0 && at && dot)
-                    updateEmail(e, id);
-                if (p.length() != 0)
-                    updatePassword(e, p);
-                 **/
+                if(ans)
+                    //updateSecurityQuestion(qOption, answer);
                 updateAccount(n, e, p, id);
             }
         });
 
+        //removes account
         RemoveAccount.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                final String e = email1.getText().toString();
-                new android.support.v7.app.AlertDialog.Builder(AccountActivity.this) //changed to MainActivity.this from context
-                        .setTitle("Remove Account")
-                        .setMessage("Are you sure you want to remove your account?")
+                //gets email associated to account
+                final String e = email;
+
+                //creates alert double checking that user wants to remove their account
+                new android.support.v7.app.AlertDialog.Builder(AccountActivity.this)
+                        .setTitle(getString(R.string.remove_account))
+                        .setMessage(getString(R.string.are_you_sure_remove))
                         .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
-                                // continue with delete
+                                // remove the account fromt he database
                                 removeAccount(e);
                             }
                         })
@@ -117,6 +128,68 @@ public class AccountActivity extends AppCompatActivity {
                         })
                         .setIcon(android.R.drawable.ic_dialog_alert)
                         .show();
+            }
+        });
+
+        //spinner for security question
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                //gets which question they selected
+                selectedQ = parent.getSelectedItem().toString();
+
+                //if it is not on "select one" prompt for an answer
+                if (!selectedQ.equals(getString(R.string.select_one))) {
+                    //alert to get answer to security question
+                    AlertDialog.Builder builder = new AlertDialog.Builder(AccountActivity.this);
+                    builder.setTitle(selectedQ);
+
+                    //assigns integer value to qOption to feed into update account because questions
+                    //are stored as integers in the database
+                    if (selectedQ.equals(getString(R.string.maiden_q)))
+                        qOption = 1;
+                    else if (selectedQ.equals(getString(R.string.food_q)))
+                        qOption = 2;
+                    else if (selectedQ.equals(getString(R.string.teacher_q)))
+                        qOption = 3;
+
+                    // Set up the input
+                    final EditText input = new EditText(AccountActivity.this);
+                    // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+                    input.setInputType(InputType.TYPE_CLASS_TEXT);
+                    builder.setView(input);
+
+                    // Set up the buttons
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            answer = input.getText().toString();
+                            //if they inputted an answer into the text field
+                            if (answer.length() > 0)
+                                ans = true;
+                                //or else prompt them to answer
+                            else {
+                                Toast.makeText(getApplicationContext(),
+                                        "Please enter valid answer to your question", Toast.LENGTH_LONG)
+                                        .show();
+                            }
+                        }
+                    });
+                    builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+
+                    builder.show();
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //do nothing
             }
         });
 
@@ -215,23 +288,15 @@ public class AccountActivity extends AppCompatActivity {
                     if (!error) {
                         //User succesfully deleted from MYSQL
 
-                            session.setLogin(false);
+                        session.setLogin(false);
 
-                            // delete all the data that was on the phone
-                            db.deleteUsers();
+                        // delete all the data that was on the phone
+                        db.deleteUsers();
 
-                            // Launching the login activity
-                            Intent intent = new Intent(AccountActivity.this, LoginActivity.class);
-                            startActivity(intent);
-                            finish();
-                        /**
-                        // Launch login activity
-                        Intent intent = new Intent(
-                                AccountActivity.this,
-                                LoginActivity.class);
+                        // Launching the login activity
+                        Intent intent = new Intent(AccountActivity.this, LoginActivity.class);
                         startActivity(intent);
                         finish();
-                         **/
                     } else {
 
                         // Error occurred in registration. Get the error
